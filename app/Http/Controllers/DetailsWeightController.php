6b373589fae5564aa51196f0903a58e4222fcf93
root <root@ThiagoRambo>
2023-04-29 00:58:10 -0300

    Final2

--- app/Http/Controllers/DetailsWeightController.php
+++ app/Http/Controllers/DetailsWeightController.php
@@ -11,8 +11,13 @@ class DetailsWeightController
     {
         $weight = new \App\Models\Weight;
         $value = $weight->first()->value;
-        return view('welcome')->with('value', $value);
-    
+        $weights = [$value,'535115','64300','543600','475010','96501','783633','789644'];
+        $tikets = ['0054','00757','00535','00112','007001','04044','004254','008796'];   
+        $drivers = ['Joao','Carlos','Henrique','Rafael','Guilherme','Thiago','Kaliton','Arlando'];
+        return view('welcome')
+            ->with('weights', $weights)
+            ->with('tikets', $tikets)
+            ->with('drivers', $drivers);
     }
     
 }
