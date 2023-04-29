f946667e9298d0c2ea286e3e4d285b3dcbfacd35
root <root@ThiagoRambo>
2023-04-29 01:06:05 -0300

    Final3

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,5 +18,11 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+
+
+        \App\Models\Weight::factory()->create([
+             'value' => '10000',
+         ]);
     }
 }
